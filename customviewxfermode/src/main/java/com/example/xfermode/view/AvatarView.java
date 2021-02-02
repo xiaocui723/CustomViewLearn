e4f3b84a509e5afd376ad82b2083c17e184187eb
xiaocui723 <xiaocui723@163.com>
2021-02-02 22:22:41 +0800

    Xfermode module 创建

---
+++ customviewxfermode/src/main/java/com/example/xfermode/view/AvatarView.java
@@ -0,0 +1,19 @@
+package com.example.xfermode.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class AvatarView extends View {
+    public AvatarView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
