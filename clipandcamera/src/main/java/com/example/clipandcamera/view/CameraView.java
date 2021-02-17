71186526e2d5cdf5669dbf30fe12f0d29fd32d4b
xiaocui723 <xiaocui723@163.com>
2021-02-17 22:45:58 +0800

    clipPath 裁切方法

--- clipandcamera/src/main/java/com/example/clipandcamera/view/CameraView.java
+++ clipandcamera/src/main/java/com/example/clipandcamera/view/CameraView.java
@@ -5,10 +5,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
 import com.example.clipandcamera.R;
 import com.example.clipandcamera.Utils;
@@ -20,13 +23,17 @@ public class CameraView extends View {
     private final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private final Bitmap bitmap = getAvatar((int) BITMAP_SIZE);
 
+    private final Path clipPath = new Path();
+
     public CameraView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.clipRect(BITMAP_PADDING, BITMAP_PADDING, BITMAP_PADDING + BITMAP_SIZE / 2, BITMAP_PADDING + BITMAP_SIZE / 2);
+        clipPath.addOval(BITMAP_PADDING, BITMAP_PADDING, BITMAP_PADDING + BITMAP_SIZE, BITMAP_PADDING + BITMAP_SIZE, Path.Direction.CCW);
+        canvas.clipPath(clipPath);
         canvas.drawBitmap(bitmap, BITMAP_PADDING, BITMAP_PADDING, paint);
     }
 
