d8dd2db2435b499a2307960937966b60a7f82717
xiaocui723 <xiaocui723@163.com>
2021-02-18 22:01:28 +0800

    ViewPropertyAnimator 使用

--- animation/src/main/java/com/example/animation/MainActivity.java
+++ animation/src/main/java/com/example/animation/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.animation;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView view = findViewById(R.id.view);
+        view.animate()
+                .translationX(Utils.dp2px(200f))
+                .translationY(Utils.dp2px(100f))
+                .alpha(0.5f)
+                .scaleX(2f)
+                .scaleY(2f)
+                .setStartDelay(1000);
     }
 }
