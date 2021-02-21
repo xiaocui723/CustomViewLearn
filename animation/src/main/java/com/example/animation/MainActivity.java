6349cd6417d3ce3ff236b5df416fbcc0a50a5985
xiaocui723 <xiaocui723@163.com>
2021-02-21 14:31:33 +0800

    动画 Module 代码优化

--- animation/src/main/java/com/example/animation/MainActivity.java
+++ animation/src/main/java/com/example/animation/MainActivity.java
@@ -107,21 +107,4 @@ public class MainActivity extends AppCompatActivity {
         animator.setDuration(10_000);
         animator.start();
     }
-
-    // 自定义估值器
-    static class PointFEvaluator implements TypeEvaluator<PointF> {
-
-        @Override
-        public PointF evaluate(float fraction, PointF startValue, PointF endValue) {
-            float startX = startValue.x;
-            float endX = endValue.x;
-            float currentX = startX + (endX - startX) * fraction;
-
-            float startY = startValue.y;
-            float endY = endValue.y;
-            float currentY = startY + (endY - startY) * fraction;
-
-            return new PointF(currentX, currentY);
-        }
-    }
 }
--- animation/src/main/java/com/example/animation/view/PointView.java
+++ animation/src/main/java/com/example/animation/view/PointView.java
@@ -1,5 +1,6 @@
 package com.example.animation.view;
 
+import android.animation.TypeEvaluator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
@@ -33,5 +34,22 @@ public class PointView extends View {
 
     public PointF getPoint() {
         return point;
+    }
+
+    // 自定义估值器
+    public static class PointFEvaluator implements TypeEvaluator<PointF> {
+
+        @Override
+        public PointF evaluate(float fraction, PointF startValue, PointF endValue) {
+            float startX = startValue.x;
+            float endX = endValue.x;
+            float currentX = startX + (endX - startX) * fraction;
+
+            float startY = startValue.y;
+            float endY = endValue.y;
+            float currentY = startY + (endY - startY) * fraction;
+
+            return new PointF(currentX, currentY);
+        }
     }
 }
